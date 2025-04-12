abfb47209320277cfb6d38586e231e9b55067f64
Shaz Rasheed Hussain <120534404+ShazRasheed13@users.noreply.github.com>
2025-04-12 18:41:04 +0530

    Time interval added - Similar to chances

---
+++ Examples/TimeInterval/TimeInterval.cs
@@ -0,0 +1,82 @@
+﻿using Examples.TimeInterval;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.TimeInterval
+{
+    public class TimeInterval
+    {
+        private readonly double _seconds; 
+        private const double Epsilon = 1e-9;
+
+        internal TimeInterval(double seconds)
+        {
+            if (seconds < 0.0)
+                throw new ArgumentException("Time interval cannot be negative");
+            _seconds = seconds;
+        }
+
+        public bool IsBetterThan(TimeInterval other) =>
+            this._seconds > other._seconds;
+
+        public override bool Equals(object? obj) =>
+            this == obj || obj is TimeInterval other && this.Equals(other);
+
+        private bool Equals(TimeInterval other) =>
+            Math.Abs(this._seconds - other._seconds) < Epsilon;
+
+        public override int GetHashCode() =>
+            Math.Round(_seconds / Epsilon).GetHashCode();
+
+        public TimeInterval Add(TimeInterval other) =>
+            new TimeInterval(this._seconds + other._seconds);
+
+        public static TimeInterval operator +(TimeInterval t1, TimeInterval t2) =>
+            t1.Add(t2);
+
+        public TimeInterval Subtract(TimeInterval other) =>
+            new TimeInterval(this._seconds - other._seconds);
+
+        public static TimeInterval operator -(TimeInterval t1, TimeInterval t2) =>
+            t1.Subtract(t2);
+
+        public TimeInterval Multiply(double factor)
+        {
+            if (factor < 0)
+                throw new ArgumentException("Cannot multiply time by negative factor");
+            return new TimeInterval(this._seconds * factor);
+        }
+
+        public static TimeInterval operator *(TimeInterval t, double factor) =>
+            t.Multiply(factor);
+
+        public double ToSeconds() => _seconds;
+        public double ToMinutes() => _seconds / 60.0;
+        public double ToHours() => _seconds / 3600.0;
+        public double ToDays() => _seconds / (24 * 3600.0);
+    }
+}
+
+namespace ExtensionMethods.Time
+{
+    public static class TimeIntervalConstructors
+    {
+        public static TimeInterval Seconds(this double value) =>
+            new TimeInterval(value);
+
+        public static TimeInterval Seconds(this int value) =>
+            new TimeInterval(value);
+
+        public static TimeInterval Minutes(this double value) =>
+            new TimeInterval(value * 60);
+
+        public static TimeInterval Hours(this double value) =>
+            new TimeInterval(value * 3600);
+
+        public static TimeInterval Days(this double value) =>
+            new TimeInterval(value * 24 * 3600);
+    }
+}
---
+++ UnitTests/TimeIntervalTests.cs
@@ -0,0 +1,66 @@
+﻿using Examples.TimeInterval;
+using ExtensionMethods.Time;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExtensionMethods.Measurement;
+
+
+namespace UnitTests
+{
+    public class TimeIntervalTests
+    {
+        private static readonly TimeInterval OneMinute = 1.0.Minutes();
+        private static readonly TimeInterval OneHour = 1.0.Hours();
+        private static readonly TimeInterval OneDay = 1.0.Days();
+
+        [Fact]
+        public void Equality()
+        {
+            Assert.Equal(60.Seconds(), 1.0.Minutes());
+            Assert.Equal(60.0.Minutes(), 1.0.Hours());
+            Assert.NotEqual(OneMinute, OneHour);
+            Assert.NotEqual(OneHour, new object());
+        }
+
+        [Fact]
+        public void Addition()
+        {
+            Assert.Equal(61.0.Minutes(), OneHour + OneMinute);
+            Assert.Equal(25.0.Hours(), OneDay + OneHour);
+        }
+
+        [Fact]
+        public void Multiplication()
+        {
+            Assert.Equal(2.0.Hours(), OneHour * 2);
+            Assert.Equal(30.0.Minutes(), OneMinute * 30);
+        }
+
+        [Fact]
+        public void Conversion()
+        {
+            var twoHours = 2.0.Hours();
+            Assert.Equal(120, twoHours.ToMinutes());
+            Assert.Equal(7200, twoHours.ToSeconds());
+            Assert.Equal(2, twoHours.ToHours());
+        }
+
+        [Fact]
+        public void Comparison()
+        {
+            Assert.True(OneHour.IsBetterThan(OneMinute));
+            Assert.True(OneDay.IsBetterThan(OneHour));
+        }
+
+        [Fact]
+        public void InvalidValues()
+        {
+            Assert.Throws<ArgumentException>(() => (-1.0).Minutes());
+            Assert.Throws<ArgumentException>(() => OneHour.Multiply(-1));
+        }
+    }
+}
