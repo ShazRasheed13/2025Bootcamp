088e17dc8cb96aad01695eb6c142f484d6c93f6d
Shaz Rasheed Hussain <120534404+ShazRasheed13@users.noreply.github.com>
2025-04-12 17:18:41 +0530

    Added game example

---
+++ Examples/Game/GameCharacter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Game
+{
+    public class GameCharacter
+    {
+        private readonly string _name;
+        private readonly int _health;
+        private const int MaxHealth = 100;
+        private const int MinHealth = 0;
+
+        public GameCharacter(string name, int health)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentException("Character must have a name");
+            if (health < MinHealth || health > MaxHealth)
+                throw new ArgumentException($"Health must be between {MinHealth} and {MaxHealth}");
+
+            _name = name;
+            _health = health;
+        }
+
+        public static GameCharacter NewWarrior(string name) =>
+            new GameCharacter(name, MaxHealth);
+
+        public GameCharacter TakeDamage(int damage)
+        {
+            if (damage <= 0)
+                throw new ArgumentException("Damage must be positive");
+            int newHealth = Math.Max(MinHealth, _health - damage);
+            return new GameCharacter(_name, newHealth);
+        }
+
+
+        public GameCharacter Heal(int amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Healing amount must be positive");
+            int newHealth = Math.Min(MaxHealth, _health + amount);
+            return new GameCharacter(_name, newHealth);
+        }
+
+        public int Health() => _health;
+
+
+        public bool IsBetterThan(GameCharacter other) =>
+            this.Health() > other.Health();
+    }
+}
---
+++ UnitTests/GameCharacter.cs
@@ -0,0 +1,47 @@
+﻿using Examples.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class GameCharacterTest
+    {
+        private readonly GameCharacter _otherCharacter = new GameCharacter("Gandalf", 80);
+        private readonly GameCharacter _warrior = GameCharacter.NewWarrior("Conan");
+
+        
+        [Fact]
+        public void TakeDamage()
+        {
+            var damagedWarrior = _warrior.TakeDamage(30);
+            Assert.Equal(100, _warrior.Health());
+            Assert.Equal(70, damagedWarrior.Health());
+        }
+
+        [Fact]
+        public void GainHealth()
+        {
+            var gainedCharacter = _otherCharacter.Heal(20);
+            Assert.Equal(100, gainedCharacter.Health());
+        }
+
+
+        [Fact]
+        public void WarriorHasBetterHealth()
+        {
+            Assert.True(_warrior.IsBetterThan(_otherCharacter));
+        }
+
+        [Fact]
+        public void ValidateParameters()
+        {
+            Assert.Throws<ArgumentException>(() => new GameCharacter("", 100));
+            Assert.Throws<ArgumentException>(() => new GameCharacter("Hero", -10));
+            Assert.Throws<ArgumentException>(() => _warrior.TakeDamage(-10));
+        }
+
+    }
+}
