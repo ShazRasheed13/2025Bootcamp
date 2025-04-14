e6e210353b2cc332bc7ac7d2f97a96e402a6417e
Mahammed Fozail <mfozail77@gmail.com>
2025-04-14 17:30:36 +0530

    ignored

--- UnitTests/obj/Debug/net9.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net9.0/UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f11d5bb123684b2a900612088b86bd46acac840")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f448b6ef6e7d7b8076d5ad287d8a243c000b68c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
